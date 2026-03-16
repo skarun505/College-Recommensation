c497d77c456cf71378cbeb9a72bd3c8d390c57a3
skarun505 <skarunkumar460@gmail.com>
2026-03-16 12:09:33 +0530

    Configure Vercel deployment: Added vercel.json, requirements.txt, and allowed .db upload

--- app.py
+++ app.py
@@ -113,7 +113,8 @@ def index():
 
     return render_template('index.html', results=results, courses=courses, districts=districts, form_data=form_data, show_results=show_results)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5001)
